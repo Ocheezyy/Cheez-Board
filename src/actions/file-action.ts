da9af2c0090d5550fbb61abe97c52db6d2d55b75
ocheezyyy <seanodonnellse@gmail.com>
2025-03-14 07:13:33 -0400

    Removed Unnecessary files.

--- src/actions/file-action.ts
+++
@@ -1,5 +0,0 @@
-import { eq, and } from "drizzle-orm";
-import { db } from "@/db";
-import { files } from "@/db/schema";
-import { File, NewFile } from "@/db/types";
-import { revalidatePath } from "next/cache";
